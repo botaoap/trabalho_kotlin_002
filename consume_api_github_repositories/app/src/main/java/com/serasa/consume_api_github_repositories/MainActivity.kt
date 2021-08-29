ad2853921743304c5ed1f28d74c43b026ef5dac8
botaoap <gabrielbotaoap@gmail.com>
2021-08-29 18:08:58 -0300

    Main

--- consume_api_github_repositories/app/src/main/java/com/serasa/consume_api_github_repositories/MainActivity.kt
+++ consume_api_github_repositories/app/src/main/java/com/serasa/consume_api_github_repositories/MainActivity.kt
@@ -2,17 +2,16 @@ package com.serasa.consume_api_github_repositories
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.serasa.consume_api_github_repositories.utils.replaceView
 import com.serasa.consume_api_github_repositories.view.MainFragment
+import com.serasa.consume_api_github_repositories.view.SearchRepositoriesFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+
+        replaceView(SearchRepositoriesFragment.newInstance())
     }
 }
