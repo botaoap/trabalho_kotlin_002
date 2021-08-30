f922d13b5291cc9b1efeb7a5b0d47a361a0e8417
botaoap <gabrielbotaoap@gmail.com>
2021-08-29 22:52:41 -0300

    Adding Delay on Response API

--- consume_api_github_repositories/app/src/main/java/com/serasa/consume_api_github_repositories/repository/RepositoryGitHubRepository.kt
+++ consume_api_github_repositories/app/src/main/java/com/serasa/consume_api_github_repositories/repository/RepositoryGitHubRepository.kt
@@ -1,5 +1,7 @@
 package com.serasa.consume_api_github_repositories.repository
 
+import android.os.Handler
+import android.os.Looper
 import com.serasa.consume_api_github_repositories.model.ItemsGitHub
 import com.serasa.consume_api_github_repositories.model.PullRequest
 import com.serasa.consume_api_github_repositories.model.RepositoryGitHub
@@ -13,60 +15,69 @@ class RepositoryGitHubRepository {
 
     private val retrofit = RetrofitBuilder.getEndpointApiGitHub()
 //    q: Char, language: String, sort: String, order: String,
-    fun fetchAllRepositories(callBack: (RepositoryGitHub?, String?) -> Unit): Boolean {
+    fun fetchAllRepositories(callBack: (RepositoryGitHub?, String?) -> Unit) {
 //        q,language,sort, order
-        retrofit.searchRepositories()
-            .enqueue(object : Callback<RepositoryGitHub>{
-                override fun onResponse(
-                    call: Call<RepositoryGitHub>,
-                    response: Response<RepositoryGitHub>
-                ) {
-                    response.body()?.let {
-                        callBack(it, null)
-                    }
-                    if (response.code() != 200) {
-                        callBack(null, "Error not recognized!!")
-                    }
-                }
 
-                override fun onFailure(call: Call<RepositoryGitHub>, t: Throwable) {
-                    callBack(null, t.localizedMessage)
-                }
+        Handler(Looper.getMainLooper()).postDelayed({
+            retrofit.searchRepositories()
+                .enqueue(object : Callback<RepositoryGitHub> {
+                    override fun onResponse(
+                        call: Call<RepositoryGitHub>,
+                        response: Response<RepositoryGitHub>
+                    ) {
+                        if (response.body() != null) {
+                            response.body()?.let {
+                                callBack(it, null)
+                            }
+                        } else {
+                            callBack(null, "Error not recognized!!")
+                        }
+                    }
 
-            })
-        return true
+                    override fun onFailure(call: Call<RepositoryGitHub>, t: Throwable) {
+                        callBack(null, t.localizedMessage)
+                    }
+
+                })
+        },2500)
     }
 
     fun fetchPullRequest(owner: String, repo: String,callBack: (List<PullRequest>?, String?) -> Unit) {
-        retrofit.getPullRequestRepository(owner, repo)
-            .enqueue(object : Callback<List<PullRequest>> {
-                override fun onResponse(call: Call<List<PullRequest>>, response: Response<List<PullRequest>>) {
-                    response.body()?.let {
-                        callBack(it, null)
+        Handler(Looper.getMainLooper()).postDelayed({
+            retrofit.getPullRequestRepository(owner, repo)
+                .enqueue(object : Callback<List<PullRequest>> {
+                    override fun onResponse(call: Call<List<PullRequest>>, response: Response<List<PullRequest>>) {
+                        if (response.body() != null) {
+                            response.body()?.let {
+                                callBack(it, null)
+                            }
+                        } else {
+                            callBack(null, "Error not recognized!!")
+                        }
                     }
 
-                    if (response.code() != 200) {
-                        callBack(null, "Error not recognized!!")
+                    override fun onFailure(call: Call<List<PullRequest>>, t: Throwable) {
+                        callBack(null, t.localizedMessage)
                     }
-                }
 
-                override fun onFailure(call: Call<List<PullRequest>>, t: Throwable) {
-                    callBack(null, t.localizedMessage)
-                }
+                })
+        }, 2500)
 
-            })
+
     }
 
     fun fetchUserDetail(username: String, callBack: (UserDetail?, String?) -> Unit) {
         retrofit.getUserDetail(username)
             .enqueue(object : Callback<UserDetail> {
                 override fun onResponse(call: Call<UserDetail>, response: Response<UserDetail>) {
-                    response.body()?.let {
-                        callBack(it, null)
-                    }
-                    if (response.code() != 200) {
+                    if (response.body() != null) {
+                        response.body()?.let {
+                            callBack(it, null)
+                        }
+                    }else {
                         callBack(null, "Error not recognized!!")
                     }
+
                 }
 
                 override fun onFailure(call: Call<UserDetail>, t: Throwable) {
